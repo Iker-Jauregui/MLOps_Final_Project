a0216d64d606c5b6d443311a2d4832c4dd85050e
Iker-Jauregui <88505263+Iker-Jauregui@users.noreply.github.com>
2025-12-17 18:54:49 +0100

    Initialize hf-space

---
+++ README.md
@@ -0,0 +1,14 @@
+---
+title: Mlops Lab2 Jauregui
+emoji: 🌖
+colorFrom: pink
+colorTo: yellow
+sdk: gradio
+sdk_version: 6.0.2
+app_file: app.py
+pinned: false
+license: mit
+short_description: Gradio APP for Lab2 of MLOps
+---
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
---
+++ app.py
@@ -0,0 +1,35 @@
+import gradio as gr
+import requests
+import numpy as np
+import cv2
+
+# URL of the API created with FastAPI
+API_URL = "https://mlops-lab2-jauregui-latest.onrender.com"
+
+
+# Function to execute when clicking the "Predict button"
+def predict(image):
+    try:
+        _, img_encoded = cv2.imencode(".jpg", image)
+        files = {"file": ("image.jpg", img_encoded.tobytes(), "image/jpeg")}
+
+        response = requests.post(f"{API_URL}/predict", files=files, timeout=60)
+        response.raise_for_status()
+        data = response.json()
+        return data.get("predicted_class")
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+
+# GUI creted using Gradio
+iface = gr.Interface(
+    fn=predict,
+    inputs=gr.Image(label="Upload Image", type="numpy", height=400),
+    outputs=gr.Textbox(label="Predicted class"),
+    title="Image class predictor with FastAPI and Gradio",
+    description="Interactive class predictor using the endpoint /predict",
+)
+
+# Launch the GUI
+if __name__ == "__main__":
+    iface.launch()
